0a188610bbcf576b5a9f04b574108687dae57c33
Kuon Tanigawa <kuonflower@gmail.com>
2020-10-23 02:37:24 +0900

    Add first commit

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    public CandyHolder candyHolder;
+    public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+
+        if(other.gameObject.tag == "Candy")
+        {
+            //指定数だけCandyのストックを増やす
+            candyHolder.AddCandy(reward);
+
+            //オブジェクトの削除
+            Destroy(other.gameObject);
+
+            if(effectPrefab != null)
+            {
+                //Candyのポジションにエフェクト
+                Instantiate(
+                    effectPrefab,
+                    other.transform.position,
+                    Quaternion.Euler(effectRotation)
+                    );
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    const int SphereCandyFrequency = 3;
+    const int MaxShotPower = 5;
+    const int RecoverySeconds = 3;
+
+    int sampleCandyCount;
+    int shotPower = MaxShotPower;
+    AudioSource shotSound;
+
+    public GameObject[] candyPrefabs;
+    public GameObject[] candySquarePrefabs;
+    //public GameObject candyHolder;
+
+    //public GameObject candyPrefab;
+
+    public CandyHolder candyHolder;
+
+    public float shotSpeed;
+    public float shotTorque;
+    public float baseWidth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shot();
+    }
+
+    //キャンディのプレファブからランダムに一つ選ぶ
+    GameObject SampleCandy()
+    {
+        GameObject prefab = null;
+
+        //特定の回数に一回丸いキャンディを選択する
+        if(sampleCandyCount % SphereCandyFrequency == 0)
+        {
+            int index = Random.Range(0, candyPrefabs.Length);
+            prefab = candyPrefabs[index];
+        }
+        else
+        {
+            int index = Random.Range(0, candySquarePrefabs.Length);
+            prefab = candySquarePrefabs[index];
+        }
+
+        sampleCandyCount++;
+
+        return prefab;
+    }
+
+    Vector3 GetInstantiatePosition()
+    {
+        //画面のサイズとｉｎｐｕｔの割合からキャンディ生成のポジションを計算
+        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        return transform.position + new Vector3(x, 0, 0);
+    }
+
+    public void Shot()
+    {
+
+        //キャンディを生成できる条件外ならばSHOTしない
+        if(candyHolder.GetCandyAmount() <= 0)
+        {
+            return;
+        }
+        if(shotPower <= 0)
+        {
+            return;
+        }
+
+        //プレファブからCandyオブジェクトを生成
+
+        GameObject candy = (GameObject)Instantiate(
+           SampleCandy(),
+           GetInstantiatePosition(),
+           Quaternion.identity
+            );
+
+        //生成したキャンディオブジェクトの親をCandyHolderに設定する
+        candy.transform.parent = candyHolder.transform;
+
+        // CandyオブジェクトのRigidbodyを取得し力と回転を加える
+        Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
+        candyRigidBody.AddForce(transform.forward * shotSpeed);
+        candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+
+        //candyのストックを消費
+        candyHolder.ConsumeCandy();
+        // ShotPowerを消費
+        ConsumePower();
+
+        //サウンドを再生
+        shotSound.Play();
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = Color.black;
+
+        //ShotPowerの残数を+の数で表示
+        string label = "";
+        for(int i = 0; i < shotPower; i++)
+        {
+            label = label += "+";
+        }
+
+        GUI.Label(new Rect(0, 15, 100, 30), label);
+    }
+
+    void ConsumePower()
+    {
+        // ShotPowerを消費すると同時に回復のカウントをスタート
+        shotPower--;
+        StartCoroutine(RecoverPower());
+    }
+
+    IEnumerator RecoverPower()
+    {
+        //一定秒数待った後にshotPowerを回復
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
+    }
+}
